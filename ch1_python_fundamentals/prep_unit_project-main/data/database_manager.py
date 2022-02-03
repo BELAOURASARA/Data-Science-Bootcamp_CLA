4836a4ecefb4879adb0e9f3a380868acabad32ab
sara <hs_belaoura@esi.dz>
2022-02-03 14:02:56 +0100

    first chapter project

---
+++ ch1_python_fundamentals/prep_unit_project-main/data/database_manager.py
@@ -0,0 +1,33 @@
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+       conn=sqlite3.connect(db_file)
+    
+    except Error as e:
+        print(e)
+    return conn 
+
+def close_connection(conn):
+    """ closes a connection to a database """
+    conn.close()
+
+
+def select_all(conn):
+    """select all rows from our table using the conn we already created """
+    cur = conn.cursor()
+    query = """SELECT * FROM longley""" 
+    cur.execute(query)
+    rows = cur.fetchall()
+    return rows 
+
+
+def print_rows(rows):
+    """ Loop through the retrived rows of a table and print them"""
+    # All of the function body is a todo task
+    for row in rows:
+        print(row)
